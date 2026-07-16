a63241a21bfd396bcf577a665953dba0699be9a4
LunaPadi01 <luanapadilha220@gmail.com>
2026-07-16 12:28:34 -0300

    Add files via upload
    Programa que recebe quatro números inteiros, calcula a soma e mostra o resultado desses números.

---
+++ soma_de_numeros.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Faça um programa que receba quatro 
+//números inteiros, calcule e mostre a soma desses números. 
+
+int main(int argc, char *argv[]) {
+	
+	int n1, n2, n3, n4, soma;
+	
+	printf("Informe o primeiro numero:\n");
+	scanf("%d", &n1);
+	printf("Informe o segundo numero:\n");
+	scanf("%d", &n2);
+	printf("Informe o terceiro numero:\n");
+	scanf("%d", &n3);
+	printf("Informe o quarto numero:\n");
+	scanf("%d", &n4);
+	
+	soma = n1 - n2 + n3 + n4;
+	
+	printf("O resultado da soma eh: %d.\n", soma);
+	system("pause");
+	
+	return 0;
+}
