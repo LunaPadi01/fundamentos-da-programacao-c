9e08afb231ea10b40e5da5483060c05bc34084f6
LunaPadi01 <luanapadilha220@gmail.com>
2026-07-17 18:25:51 -0300

    Add files via upload
    Programa que recebe dois números do usuario e identifica qual deles é o maior, mostrando na tela.

---
+++ verifica_maior_numero.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	  
+	  //Faça um programa que peça dois números ao usuário e mostre qual o maior e qual o menor
+	
+	int x, y;
+	
+	printf("Insira o primeiro numero:");
+	scanf("%i", &x);
+	
+	printf("Insira o segundo numero:");
+	scanf("%i", &y);
+	
+	if (x > y){
+		printf("primeiro numero eh maior");
+}   else {
+	printf("segundo numero eh maior");
+}
+	
+	
+		}
+	
